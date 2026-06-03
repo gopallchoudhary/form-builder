f9ce8db2aa33b2fb4412e9674b5d2fc92bfd4e63
gopallchoudhary <gopalchoudhary521@gmail.com>
2026-06-03 17:31:20 +0530

    feat: add form_fields table and related schema updates

---
+++ packages/database/models/form-field.ts
@@ -0,0 +1,39 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  numeric,
+  boolean,
+  text,
+  pgEnum,
+  unique
+} from "drizzle-orm/pg-core";
+import { formsTable } from "./form";
+
+export const fieldTypeEnum = pgEnum('field_type_enum', ['TEXT', 'NUMBER', 'EMAIL', 'YES_NO', 'PASSWORD'])
+
+
+export const formFieldsTable = pgTable('form_fields', {
+  id: uuid('id').primaryKey().defaultRandom(),
+
+  fieldLabel: varchar('field_label', {length: 100}).notNull(),
+  fieldKey: varchar('field_key', {length: 50}).notNull(),
+
+  placeholder: text('placeholder'),
+  description: text('description'),
+
+  index: numeric('index', {scale: 2}),
+  isRequired: boolean('is_required').default(false).notNull(),
+
+  type: fieldTypeEnum('type').notNull(),
+
+  formId: uuid('form_id').references(() => formsTable.id),
+
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+}, (table) => {
+          return {
+            uniqueFormIdAndIndex: unique().on(table.formId, table.index)
+          }
+})
