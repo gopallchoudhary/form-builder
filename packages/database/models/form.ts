9af3464dbec4460e4c40124ef172f95bf1d81b2c
gopallchoudhary <gopalchoudhary521@gmail.com>
2026-06-04 06:44:03 +0530

    change: db model

--- packages/database/models/form.ts
+++ packages/database/models/form.ts
@@ -8,15 +8,15 @@ import { usersTable } from "./user";
 
 
 export const formsTable = pgTable('forms', {
-    id: uuid('id').primaryKey().defaultRandom(),
-    title: varchar('title', {length: 55}).notNull(),
-    description: varchar('description', {length: 300}),
+  id: uuid('id').primaryKey().defaultRandom(),
+  title: varchar('title', { length: 55 }).notNull(),
+  description: varchar('description', { length: 300 }),
 
-    createdBy: uuid('created_by').references(() => usersTable.id),
+  createdBy: uuid('created_by').notNull().references(() => usersTable.id),
 
 
-    createdAt: timestamp("created_at").defaultNow(),
-    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
 
 
 })
